561551a14785ffd0228502816b5f8a64a784a9e6
MichalPajestka <001michal002@gmail.com>
2023-09-06 15:29:20 +0200

    -Created bank branch -Added "Get a loan" button -Created a prompt for entering loan amount

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let totalBalance = "200";
 document.getElementById("accountBalance").innerHTML = new Intl.NumberFormat('no-NO', {style: 'currency', currency: 'NOK' }).format(totalBalance);
 
-
+function getALoan() {
+    let outstandingLoan = "";
+    let  loan = prompt("Enter amount: ");
+    if(loan == null || loan == "") {
+        alert("Invalid amount");
+    } else {
+        outstandingLoan = "Your loan: " + loan;
+    }
+    document.getElementById("outstanding-loan").innerHTML = outstandingLoan;
+}
