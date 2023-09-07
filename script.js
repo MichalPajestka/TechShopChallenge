99e2784e71259f1dc23937cd04e208b48d193041
MichalPajestka <001michal002@gmail.com>
2023-09-07 08:51:25 +0200

    -Fix: Get loan function now works properly

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let totalBalance = "200";
+let totalBalance = 200;
+let outstandingLoan = 0;
 document.getElementById("accountBalance").innerHTML = new Intl.NumberFormat('no-NO', {style: 'currency', currency: 'NOK' }).format(totalBalance);
 
 function getALoan() {
-    let outstandingLoan = "";
     let loan = parseFloat(prompt("Enter amount:"));
-    if(isNaN(loan) || loan <= 0 || loan > totalBalance * 2) {
+    if (isNaN(loan) || loan <= 0 || loan > totalBalance * 2) {
         alert("Invalid amount");
+    } else if (outstandingLoan > 0) {
+        alert("You need to repay your previous loan first")
     } else {
-        outstandingLoan = "Your loan: " + loan;
+        // Update the outstanding loan
+        outstandingLoan = loan;
+        document.getElementById("outstanding-loan").innerHTML = "Your outstanding loan: " + new Intl.NumberFormat('no-NO', {style: 'currency', currency: 'NOK' }).format(outstandingLoan);
+        
+         // Update the total balance and display
+         totalBalance += loan;
+         document.getElementById("accountBalance").innerHTML = new Intl.NumberFormat('no-NO', {style: 'currency', currency: 'NOK' }).format(totalBalance);
     }
-    document.getElementById("outstanding-loan").innerHTML = outstandingLoan;
+    
 }
